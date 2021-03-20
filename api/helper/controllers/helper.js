fbb630c4d0e746cd43197ac9529a169e5ba120af
sujal-ux <sumo12082000@gmail.com>
2021-03-20 14:43:28 +0530

    Helpers end point

--- api/helper/controllers/helper.js
+++ api/helper/controllers/helper.js
@@ -1,8 +1,34 @@
 'use strict';
+const geolib = require('geolib');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+var jobarray
+module.exports = {
+  async fetchJobs(ctx){
+    var lat = ctx.query.latitude
+    var long = ctx.query.longitude
+    var rad = ctx.query.radius || 5;
 
-module.exports = {};
+    if(lat==null || long==null){
+      return ctx.send({
+        status: 400, //bad request
+        message: "provide lat and long"
+      });
+    }
+
+    var jobs = await strapi.query("job").find({
+      isDone: false
+    })
+
+    await jobs.map(job=>{
+      if(geolib.getDistance({lat,long},{job.latitude,job.longitude})<rad){
+        jobarray.push(job);
+      }
+    })
+
+    return ctx.send({
+      status: 200,
+      jobs: jobarray
+    })
+
+  }
+};
